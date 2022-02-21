cf4ffe4a7ca8591605dd87ee8928dbe4e8185834
Alexandre Gautier <alexandre.gautier@epita.fr>
2022-02-21 22:40:29 +0100

    suce

--- stdin_to_file/stdin-to-file.cc
+++ stdin_to_file/stdin-to-file.cc
@@ -3,9 +3,13 @@
 
 void stdin_to_file()
 {
+    std::ofstream file_out("file.out");
     std::string line;
-    while (std::getline(std::cin, line)) // input from the file in.txt
+
+    while (std::cin >> line) // input from the file in.txt
     {
-        std::cout << line << "\n"; // output to the file out.txt
+        file_out << line << "\n"; // output to the file out.txt
     }
+
+    file_out.close();
 }
