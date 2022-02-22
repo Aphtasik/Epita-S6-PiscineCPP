8dfd0de8377fb6b549f0ba8d875900f3ce0d61df
Alexandre Gautier <alexandre.gautier@epita.fr>
2022-02-22 18:40:51 +0100

    hello

--- directories_infos/read-info.cc
+++ directories_infos/read-info.cc
@@ -4,13 +4,7 @@
 
 DirectoryInfo* readInfo(std::ifstream&)
 {
-    std::string name;
-    unsigned int size;
-    std::string owner;
-    unsigned int rights;
-        
-    int i = 0;
-
+    return nullptr;
 }
 
 
