970ba73b186919b5d51674f708e31f29dfc7fdac
Alexandre Gautier <alexandre.gautier@epita.fr>
2022-02-23 14:15:59 +0100

    int cont

---
+++ exception/player.cc
@@ -0,0 +1,10 @@
+#include "player.hh"
+
+#include <string>
+#include <iostream>
+#include <exception>
+
+Player::Player(const std::string& name, unsigned int age)
+{
+    if (std::is_empty<std::string>(name))
+}
--- int_container/int-container.cc
+++ int_container/int-container.cc
@@ -9,7 +9,7 @@ MyIntContainer::MyIntContainer(size_t size)
 
 void MyIntContainer::print() const
 {
-    for (int i = 0; i < current_size_ - 1; i++)
+    for (long unsigned int i = 0; i < current_size_ - 1; i++)
     {
         std::cout << elems_[i] << " | ";
     }
@@ -27,12 +27,73 @@ bool MyIntContainer::add(int elem)
     if (current_size_ == max_size_)
         return false;
 
-    current_size_++;
     elems_[current_size_] = elem;
+    current_size_++;
     return true;
 }
 
 std::optional<int> MyIntContainer::pop()
 {
-    
+    if (current_size_ == 0)
+        return std::nullopt;
+
+    current_size_--;
+    return std::optional<int>(elems_[current_size_]);
+}
+
+std::optional<int> MyIntContainer::get(size_t position) const
+{
+    if (position > current_size_ - 1)
+        return std::nullopt;
+
+    return std::optional<int>(elems_[position]);
+}
+
+std::optional<size_t> MyIntContainer::find(int elem) const
+{
+    for (size_t i = 0; i < current_size_; i++)
+    {
+        if (elems_[i] == elem)
+            return std::optional<size_t>(i);
+    }
+    return std::nullopt;
+}
+
+void swap(int* xp, int* yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+// A function to implement bubble sort
+void bubbleSort(std::unique_ptr<int[]>& arr, int n)
+{
+    int i, j;
+    for (i = 0; i < n - 1; i++)
+
+        // Last i elements are already in place
+        for (j = 0; j < n - i - 1; j++)
+            if (arr[j] > arr[j + 1])
+                swap(&arr[j], &arr[j + 1]);
+}
+
+void MyIntContainer::sort()
+{
+    bubbleSort(elems_, current_size_);
+}
+
+bool MyIntContainer::is_sorted() const
+{
+    if (current_size_ == 1)
+        return true;
+
+    int prec = elems_[0];
+    for (long unsigned int i = 1; i < current_size_; i++)
+    {
+        if (elems_[i] < prec)
+            return false;
+        prec = elems_[i];
+    }
+    return true;
 }
