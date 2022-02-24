c5408b47390acf1d3fc5696560f78236ec218722
Alexandre Gautier <alexandre.gautier@epita.fr>
2022-02-24 17:16:59 +0100

    forward

---
+++ game_theory/lambdas.cc
@@ -0,0 +1,26 @@
+#include "lambdas.hh"
+
+strategy_type cooperator()
+{
+
+}
+strategy_type cheater()
+{
+
+}
+strategy_type copycat()
+{
+
+}
+strategy_type grudger()
+{
+
+}
+strategy_type detective()
+{
+
+}
+strategy_type copykitten()
+{
+
+}
