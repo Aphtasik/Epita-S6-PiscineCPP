77ad4ac663c45e65bc908e0efefdfd43d2cb9704
Alexandre Gautier <alexandre.gautier@epita.fr>
2022-02-24 18:00:11 +0100

    fore

--- for_each/for-each.hh
+++ for_each/for-each.hh
@@ -1,14 +1,11 @@
-#pragma once
-
-#include <cmath>
 #include <iostream>
 
-template <class Iterator>
-void my_foreach(Iterator i1, Iterator i2, std::is_function<Iterator> f)
+template <typename It, typename F>
+void my_foreach(It i1, It i2, F f)
 {
     while (i1 != i2)
     {
-        *i1 = f(*i1);
-        i1++;
+        f(*i1);
+        ++i1;
     }
 }
