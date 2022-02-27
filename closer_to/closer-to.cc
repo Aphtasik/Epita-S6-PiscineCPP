649d3d87775dbd3502004d70e5b73f007d55b2e7
Alexandre Gautier <alexandre.gautier@epita.fr>
2022-02-27 19:29:16 +0100

    close

--- closer_to/closer-to.cc
+++ closer_to/closer-to.cc
@@ -1,3 +1,12 @@
 #include "closer-to.hh"
 
+#include <algorithm>
+
+CloserTo::CloserTo(int i)
+    : i_(i){};
+
+bool CloserTo::operator()(const int& a, const int& b) const
+{
+    return (i_ - a >= b - i_);
+}
 
