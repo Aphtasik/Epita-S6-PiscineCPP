562b2ce7e88fbd3febc872c26824d2bd485fefe4
Alexandre Gautier <alexandre.gautier@epita.fr>
2022-03-15 09:02:55 +0100

    impl

--- implements/implements.hh
+++ implements/implements.hh
@@ -1,12 +1,58 @@
 #pragma once
 
-
 // Requires clause used in a concept
-template <typename T>
-concept impl_basic_op = requires
+template <typename A, typename B = A>
+concept impl_basic_op = requires(A a, B b)
 { // Requirement sequence
-    T::operator+;
-    T::operator-;
-    T::operator*;
-    T::operator/;
+    {
+        +a
+        } -> std::convertible_to<A>;
+    {
+        -a
+        } -> std::convertible_to<A>;
+    {
+        a + b
+        } -> std::convertible_to<A>;
+    {
+        a - b
+        } -> std::convertible_to<A>;
+    {
+        a* b
+        } -> std::convertible_to<A>;
+    {
+        a / b
+        } -> std::convertible_to<A>;
 };
+
+template <typename A, typename B = A>
+concept impl_modulo = requires(A a, B b)
+{
+    { a % b };
+};
+
+template <typename A, typename B = A>
+concept impl_bitwise_op = requires(A a, B b)
+{
+    {
+        a& b
+        } -> std::convertible_to<A>;
+    {
+        a | b
+        } -> std::convertible_to<A>;
+    {
+        a ^ b
+        } -> std::convertible_to<A>;
+    {
+        ~a
+        } -> std::convertible_to<A>;
+    {
+        a << b
+        } -> std::convertible_to<A>;
+    {
+        a >> b
+        } -> std::convertible_to<A>;
+};
+
+template <typename A, typename B = A>
+concept impl_arith_op =
+    impl_basic_op<A, B> && impl_modulo<A, B> && impl_bitwise_op<A, B>;
