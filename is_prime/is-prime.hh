2ef7ea32bdd219ebcbef343f9b5c4fa347390511
Alexandre Gautier <alexandre.gautier@epita.fr>
2022-03-15 15:27:17 +0100

    prime

---
+++ is_prime/is-prime.hh
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <cmath>
+
+template <int n>
+constexpr bool is_prime()
+{
+    if (n <= 1)
+        return false;
+
+    for (int i = 2; i < n; i++)
+        if (n % i == 0)
+            return false;
+
+    return true;
+}
+
