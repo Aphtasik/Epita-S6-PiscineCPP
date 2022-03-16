94783c807c3cbc745acb2c75a7be2ec4f8dc3987
Alexandre Gautier <alexandre.gautier@epita.fr>
2022-03-16 19:21:49 +0100

    func

--- exist_functor/exist.hxx
+++ exist_functor/exist.hxx
@@ -4,6 +4,9 @@ template <class T>
 bool Exist<T>::operator()(T t)
 {
     if (std::find(vec.begin(), vec.end(), t) != vec.end())
+    {
+        vec.push_back(t);
         return true;
+    }
     return false;
 }
