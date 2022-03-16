0172b5b8a857211e6506d5ab0b900f5fa83d6050
Alexandre Gautier <alexandre.gautier@epita.fr>
2022-03-16 18:54:17 +0100

    const

--- constexpr_factorial/constexpr-factorial.hh
+++ constexpr_factorial/constexpr-factorial.hh
@@ -1,6 +1,6 @@
 constexpr unsigned long long factorial(unsigned int n)
 {
-    unsigned long res = 0;
+    unsigned long res = i;
     for (unsigned int i = 1; i <= n; i++)
         res *= i;
     return res;
