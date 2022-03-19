3fff3dd7798b1012107d551e18cd3a422d0cc779
Alexandre Gautier <alexandre.gautier@epita.fr>
2022-03-19 22:55:00 +0100

    dir

--- directories_infos/read-info.cc
+++ directories_infos/read-info.cc
@@ -1,10 +1,26 @@
-#include <fstream>
 #include "read-info.hh"
+
 #include "directory-info.hh"
 
-DirectoryInfo* readInfo(std::ifstream&)
+DirectoryInfo* read_info(std::ifstream& file)
 {
-    return nullptr;
+    if (file.eof())
+        return nullptr;
+    if (!file.is_open())
+        return nullptr;
+
+    std::string line, name, owner;
+    unsigned int size, rights;
+
+    std::getline(file, line);
+    std::stringstream ss;
+    ss.str(line);
+
+    if(!(ss >> name >> std::dec >> size >> std::oct >> rights >> owner));
+        return nullptr;
+    if (file.peek() == '\n' || !file.eof())
+        return nullptr;
+
+    DirectoryInfo d = DirectoryInfo(name, size, rights, owner);
+    return &d;
 }
-
-
--- directories_infos/read-info.hh
+++ directories_infos/read-info.hh
@@ -1,3 +1,12 @@
 #pragma once
 
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <istream>
+#include <sstream>
+#include <string>
 
+#include "directory-info.hh"
+
+DirectoryInfo* read_info(std::ifstream& file);
