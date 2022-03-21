431d20eccf788c229bfbabcc12da1eadabdea53b
Alexandre Gautier <alexandre.gautier@epita.fr>
2022-03-21 14:52:21 +0100

    dir

--- directories_infos/read-info.cc
+++ directories_infos/read-info.cc
@@ -4,21 +4,21 @@
 
 DirectoryInfo* read_info(std::ifstream& file)
 {
-    if (file.eof())
-        return nullptr;
-    if (!file.is_open())
+    if (!file.is_open() || file.eof())
         return nullptr;
 
-    std::string line, name, owner;
-    unsigned int size, rights;
+    std::string line;
+    std::string name;
+    std::string owner;
+    unsigned int size;
+    unsigned int rights;
 
     std::getline(file, line);
     std::stringstream ss(line);
 
     if (!(ss >> name >> std::dec >> size >> std::oct >> rights >> owner))
         return nullptr;
-    if (!file.eof())
+    if (!ss.eof())
         return nullptr;
-
     return new DirectoryInfo(name, size, rights, owner);
 }
